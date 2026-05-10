f3d87d975098213e3bfaf83bda4cf72fe7d6bf9e
Adain Rivers <93623214+adainrivers@users.noreply.github.com>
2026-05-10 17:30:19 +0200

    Split Tauri host code by domain

---
+++ app/src-tauri/src/models.rs
@@ -0,0 +1,13 @@
+pub mod battlegroup;
+pub mod common;
+pub mod config;
+pub mod host_vm;
+pub mod manager;
+pub mod workloads;
+
+pub use battlegroup::*;
+pub use common::*;
+pub use config::*;
+pub use host_vm::*;
+pub use manager::*;
+pub use workloads::*;
